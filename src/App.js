bc33954918e02793e9a51730ce5b3d5e44306b14
Chris00-lab <christelgabriel00@gmail.com>
2023-01-18 06:44:08 +0200

    update code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to</h1> <br/>
+      <h2>REACT APP</h2>
+    </div>
+  );
+}
+
+export default App;
