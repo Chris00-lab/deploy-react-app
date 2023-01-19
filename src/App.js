e7a059d4d71117e9dbe7794d54ae3006166d1079
Chris00-lab <59288328+Chris00-lab@users.noreply.github.com>
2023-01-19 17:44:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to</h1> <br/>
+      <h1>Hello to</h1> <br/>
       <h2>REACT APPLICATION</h2>
     </div>
   );
