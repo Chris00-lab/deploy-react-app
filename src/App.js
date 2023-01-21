c5f1e2a6ae96ec22c9853428801bb3a99a41db13
Chris00-lab <christelgabriel00@gmail.com>
2023-01-21 08:07:59 +0200

    maj code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello to</h1> <br/>
+      <h1>Welcome to</h1> <br/>
       <h2>REACT APPLICATION</h2>
     </div>
   );
