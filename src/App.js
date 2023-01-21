a6a63ba71561f8a7c48800fd48f8e4f3167e56b7
Chris00-lab <christelgabriel00@gmail.com>
2023-01-21 08:39:07 +0200

    hey

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello to</h1> <br/>
+      <h1>Hey to</h1> <br/>
       <h2>REACT APPLICATION</h2>
     </div>
   );
